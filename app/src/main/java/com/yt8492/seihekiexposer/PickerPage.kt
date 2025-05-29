e8a268d04421761bdd275900818b56c374b88144
mayamito <tomiyama-yuta2@dmm.com>
2025-05-30 03:22:54 +0900

    ローカルで動くまで

--- app/src/main/java/com/yt8492/seihekiexposer/PickerPage.kt
+++ app/src/main/java/com/yt8492/seihekiexposer/PickerPage.kt
@@ -2,6 +2,7 @@ package com.yt8492.seihekiexposer
 
 import android.content.Intent
 import android.provider.OpenableColumns
+import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
@@ -13,9 +14,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.documentfile.provider.DocumentFile
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.websocket.WebSockets
+import io.ktor.client.plugins.websocket.webSocket
+import io.ktor.client.plugins.websocket.ws
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import io.ktor.websocket.Frame
+import io.ktor.websocket.readText
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Composable
 fun PickerPage() {
@@ -26,6 +40,13 @@ fun PickerPage() {
   val (directories, setDirectories) = remember {
     mutableStateOf(emptyList<String>())
   }
+  val client = remember {
+    HttpClient(OkHttp) {
+      install(WebSockets)
+      install(ContentNegotiation)
+    }
+  }
+  val coroutineScope = rememberCoroutineScope()
   val launcher = rememberLauncherForActivityResult(ActivityResultContracts.OpenDocumentTree()) { uri ->
     uri?.let {
       contentResolver.takePersistableUriPermission(
@@ -37,6 +58,31 @@ fun PickerPage() {
           if (file.isDirectory) {
             file.name?.let {
               setDirectories(directories + it)
+            }
+          }
+        }
+      }
+      coroutineScope.launch(Dispatchers.IO) {
+        client.ws("ws://10.0.2.2:8080/ws") {
+          // Receive a response from the WebSocket server
+          while (true) {
+            val frame = incoming.receive()
+            if (frame is Frame.Text && frame.readText() == "get") {
+              val response = with(Dispatchers.Main) {
+                DocumentFile.fromTreeUri(context, uri)
+                  ?.listFiles()
+                  ?.filter { it.isDirectory && it.name?.startsWith(".") == false }
+                  ?.joinToString(
+                    separator = ",",
+                    prefix = "{",
+                    postfix = "}",
+                  ) {
+                    "\"${it.name}\""
+                  }
+                  ?: "{}"
+              }
+              // Send the response back to the WebSocket server
+              outgoing.send(Frame.Text(response))
             }
           }
         }
--- server/src/main/kotlin/com/yt8492/seihekiexposer/server/Application.kt
+++ server/src/main/kotlin/com/yt8492/seihekiexposer/server/Application.kt
@@ -18,45 +18,75 @@ import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 
 fun main() {
-    embeddedServer(Netty, port = 8080) {
-      val mutex = Mutex()
-      var wsConnection: WebSocketSession? = null
+  embeddedServer(Netty, port = 8080) {
+    val mutex = Mutex()
+    var wsConnection: WebSocketSession? = null
 
-      install(WebSockets)
+    install(WebSockets)
 
-      routing {
-        // WebSocketエンドポイント
-        webSocket("/ws") {
-          mutex.withLock {
-            wsConnection?.close()
-            wsConnection = this
-          }
+    routing {
+      // WebSocketエンドポイント
+      webSocket("/ws") {
+        println("connected to WebSocket")
+        mutex.withLock {
+          wsConnection = this
         }
-
-        // HTTP GETエンドポイント
-        get("/sh") {
-          val connection = mutex.withLock { wsConnection }
-          if (connection == null) {
-            call.respondText("WebSocket not connected", status = HttpStatusCode.ServiceUnavailable)
-            return@get
-          }
-
+        while (true) {
           try {
-            connection.send(Frame.Text("get"))
-            val responseText = (connection.incoming.receive() as? Frame.Text)?.readText()
-            if (responseText != null) {
-              call.respondText(responseText, contentType = ContentType.Application.Json)
-            } else {
-              call.respondText("No response from WebSocket", status = HttpStatusCode.InternalServerError)
+            val frame = incoming.receive()
+            if (frame is Frame.Text) {
+              val text = frame.readText()
+              println("Received: $text")
+              // Echo the received message back to the client
+              if (text == "end") {
+                println("Closing WebSocket connection")
+                mutex.withLock {
+                  wsConnection?.close()
+                  wsConnection = null
+                }
+                break
+              } else {
+                send(Frame.Text("get"))
+              }
             }
           } catch (e: Exception) {
+            e.printStackTrace()
             mutex.withLock {
-              connection.close()
+              wsConnection?.close()
               wsConnection = null
             }
-            call.respondText("WebSocket error", status = HttpStatusCode.InternalServerError)
+            break
           }
         }
       }
+
+      // HTTP GETエンドポイント
+      get("/sh") {
+        val connection = mutex.withLock { wsConnection }
+        if (connection == null) {
+          call.respondText("WebSocket not connected", status = HttpStatusCode.ServiceUnavailable)
+          return@get
+        }
+
+        try {
+          println("send")
+          connection.send(Frame.Text("get"))
+          println("sent")
+          val responseText = (connection.incoming.receive() as? Frame.Text)?.readText()
+          if (responseText != null) {
+            call.respondText(responseText, contentType = ContentType.Application.Json)
+          } else {
+            call.respondText("No response from WebSocket", status = HttpStatusCode.InternalServerError)
+          }
+        } catch (e: Exception) {
+          e.printStackTrace()
+          mutex.withLock {
+            connection.close()
+            wsConnection = null
+          }
+          call.respondText("WebSocket error", status = HttpStatusCode.InternalServerError)
+        }
+      }
     }
+  }.start(wait = true)
 }
