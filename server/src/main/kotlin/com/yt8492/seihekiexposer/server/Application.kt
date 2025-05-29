cc7ca19b797f9040c8cb4c00b85b1dcd6fbf4778
mayamito <tomiyama-yuta2@dmm.com>
2025-05-30 01:56:14 +0900

    サーバー実装

--- server/src/main/kotlin/com/yt8492/seihekiexposer/server/Application.kt
+++ server/src/main/kotlin/com/yt8492/seihekiexposer/server/Application.kt
@@ -1,22 +1,62 @@
 package com.yt8492.seihekiexposer.server
 
-import io.ktor.server.application.Application
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.install
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 import io.ktor.server.websocket.WebSockets
 import io.ktor.server.websocket.webSocket
+import io.ktor.websocket.Frame
+import io.ktor.websocket.WebSocketSession
+import io.ktor.websocket.close
+import io.ktor.websocket.readText
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
-fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
-}
+fun main() {
+    embeddedServer(Netty, port = 8080) {
+      val mutex = Mutex()
+      var wsConnection: WebSocketSession? = null
 
-@Suppress("unused")
-fun Application.module() {
-  install(WebSockets)
+      install(WebSockets)
 
-  routing {
-    webSocket("/ws") {
+      routing {
+        // WebSocketエンドポイント
+        webSocket("/ws") {
+          mutex.withLock {
+            wsConnection?.close()
+            wsConnection = this
+          }
+        }
 
+        // HTTP GETエンドポイント
+        get("/sh") {
+          val connection = mutex.withLock { wsConnection }
+          if (connection == null) {
+            call.respondText("WebSocket not connected", status = HttpStatusCode.ServiceUnavailable)
+            return@get
+          }
+
+          try {
+            connection.send(Frame.Text("get"))
+            val responseText = (connection.incoming.receive() as? Frame.Text)?.readText()
+            if (responseText != null) {
+              call.respondText(responseText, contentType = ContentType.Application.Json)
+            } else {
+              call.respondText("No response from WebSocket", status = HttpStatusCode.InternalServerError)
+            }
+          } catch (e: Exception) {
+            mutex.withLock {
+              connection.close()
+              wsConnection = null
+            }
+            call.respondText("WebSocket error", status = HttpStatusCode.InternalServerError)
+          }
+        }
+      }
     }
-  }
 }
